9985be6b108e74b5b76fce976abe8c0fe7861215
Chris Nelson <superchrisnelson@gmail.com>
2016-01-20 19:08:27 -0500

    Added ui router and home page route

--- client/app/app.js
+++ client/app/app.js
@@ -1,3 +1,12 @@
 import angular from "angular";
+import uiRouter from "angular-ui-router";
+import homeTemplate from "./home.html";
 
-let module = angular.module("hirem", []);
+let module = angular.module("hirem", [uiRouter]);
+
+module.config( ($stateProvider) => {
+  $stateProvider.state("home", {
+    url: "/",
+    template: homeTemplate
+  })
+});
