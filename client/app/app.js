4d9fb9d4ce55bc693e2ef2718b48b9a7d1a9ef3a
Chris Nelson <superchrisnelson@gmail.com>
2016-02-03 15:32:24 -0500

    start point for filter lab

--- client/app/app.js
+++ client/app/app.js
@@ -3,8 +3,9 @@ import uiRouter from "angular-ui-router";
 import homeTemplate from "./home.html";
 import aboutTemplate from "./about.html";
 import candidatesModule from "./candidates/module";
+import filtersModule from "./filters/module";
 
-let module = angular.module("hirem", [uiRouter, candidatesModule.name]);
+let module = angular.module("hirem", [uiRouter, candidatesModule.name, filtersModule.name]);
 
 module.config( ($stateProvider, $urlRouterProvider) => {
   $urlRouterProvider.otherwise('/');
