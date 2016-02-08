ed31bcfb4ed15a4f08e40510d4ec9bc7ed42f6ff
Chris Nelson <superchrisnelson@gmail.com>
2016-02-08 13:44:51 -0500

    fix start point for lab4

--- client/app/filters/helloFilter.js
+++ client/app/filters/helloFilter.js
@@ -1,5 +1,9 @@
-export default function($sce) {
+let helloFilter = function($sce) {
   return function(text, more) {
     return $sce.trustAsHtml("<b>Hello</b>" + more + text);
   };
 }
+
+helloFilter.$inject = ["$sce"];
+
+export default helloFilter;
