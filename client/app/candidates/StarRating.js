1f35bcf04c52f70b618ca0f77de5af0d1c798c31
Chris Nelson <superchrisnelson@gmail.com>
2016-02-09 15:55:49 -0500

    lab 8 solution

--- client/app/candidates/StarRating.js
+++ client/app/candidates/StarRating.js
@@ -1,7 +1,14 @@
 let starRating = () => {
   return {
+    scope: {
+      starRating: "="
+    },
     link: (scope, element, attrs) => {
       $(element).raty({path: "/images"});
+      scope.$watch("starRating", (rating) => {
+        $(element).raty("score", rating);
+      });
+
     }
   };
 };
